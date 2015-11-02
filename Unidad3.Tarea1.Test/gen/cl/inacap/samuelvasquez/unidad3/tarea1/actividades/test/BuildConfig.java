b8938f9190f5fed8e9a2d65536721ad9ca6f7c99
samuelvasquez <samuelvasquez@gmail.com>
2015-11-02 03:12:12 -0300

    1.8. Test a clase DAOCliente

---
+++ Unidad3.Tarea1.Test/gen/cl/inacap/samuelvasquez/unidad3/tarea1/actividades/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cl.inacap.samuelvasquez.unidad3.tarea1.actividades.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
